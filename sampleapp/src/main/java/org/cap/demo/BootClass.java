0a7e536fa447188036d0ef37f09bdfcc8d975bb7
ManognaReddy4a3115 <manognareddy115@gmail.com>
2021-09-20 10:56:22 +0530

    show method added

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -3,7 +3,10 @@ package org.cap.demo;
 public class BootClass {
 public static void main(String[] args) {
 	System.out.println("Hello from sts");
+show();
 
-
+}
+public static void show() {
+	System.out.println("show method here");
 }
 }
